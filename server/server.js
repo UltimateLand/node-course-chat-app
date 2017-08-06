f505622894a45cad88358d86ac668e9b472343b7
Jun-Ren Chen <Jun-Ren, Chen>
2017-08-06 17:10:26 +0800

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -14,14 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'Horace',
-    text: 'Do not forget to watch RTL at 7 pm.',
-    createdAt: 123
-  });
-
-  socket.on('createMessage', (newMessage) => {
-    console.log('You got a message:', newMessage);
+  socket.on('createMessage', (message) => {
+    console.log('createMessage:', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', () => {
